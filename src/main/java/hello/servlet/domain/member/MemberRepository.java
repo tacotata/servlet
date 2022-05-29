81ebd12d90dbe481e8ef4127f2abe54aeaf21287
tacotata <71251163+tacotata@users.noreply.github.com>
2022-05-29 16:26:37 +0900

    Model 추가 - v3

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -9,7 +9,7 @@ import java.util.Map;
  * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
  */
 public class MemberRepository {
-//  싱그톤으로해서 static 없어도 되긴함
+//  싱글톤으로해서 static 없어도 되긴함
     private static Map<Long, Member> store = new HashMap<>();
     private static Long sequence = 0L;
 
